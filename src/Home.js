4e104951ae5f149feb6d06ba7433e24acc908092
akash aks <akashsah.aks@gmail.com>
2023-02-23 22:35:58 +0530

    initial blog starting from now

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  const handleClick = (e) => {
-    console.log("Hello Ninjas", e);
-  };
-  const handleClickAgain = (name, e) => {
-    console.log("Hello Ninjas " + name, e.target, e.target.value);
-  };
   return (
     <>
       <div className="home">
         <h2>Homepage Component</h2>
-        <button onClick={handleClick}>Click me</button>
-        <button
-          value="submit"
-          onClick={(e) => {
-            handleClickAgain("aks", e);
-          }}
-        >
-          Click me again
-        </button>
       </div>
     </>
   );
