a4d19be621e16412654c890a3fc269fb019e3a17
akash aks <akashsah.aks@gmail.com>
2023-02-23 22:57:40 +0530

    used pops object and rendered it

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import { React, useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome Party", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    { title: "Web dev tips", body: "lorem ipsum...", author: "mario", id: 3 },
+  ]);
   return (
     <>
       <div className="home">
-        <h2>Homepage Component</h2>
+        <BlogList blogs={blogs} title="All Blogs" />
       </div>
     </>
   );
