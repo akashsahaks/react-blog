a563a546110159f1688782c4e331aaca9fe75857
akash aks <akashsah.aks@gmail.com>
2023-02-24 00:01:09 +0530

    added setTimeout to simulate delay and showing loading compnent

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,31 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((respose) => {
-        return respose.json();
-      })
-      .then((data) => {
-        // console.log(data);
-        setBlogs(data);
-      });
+    //  In real life sometimes fetching data takes time for sometimes, at that time our
+    //  loading component show as much time takes server to respond data after responding loading
+    //  compoennt gets unload, so here our api does not taking time to load data from server so just to
+    //  demonstrate a loading component how its working that's why we use setTimeout for 1 sec delay
+
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((respose) => {
+          return respose.json();
+        })
+        .then((data) => {
+          // console.log(data);
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 1000);
   }, []);
+
   return (
     <>
       <div className="home">
+        {isPending && <div>Loading...</div>}
         {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
       </div>
     </>
