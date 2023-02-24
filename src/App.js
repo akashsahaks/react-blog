9511d68b4a347c47231ed48b0bfd19ff1f582cce
akash aks <akashsah.aks@gmail.com>
2023-02-24 13:04:09 +0530

    added notfound page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<Create />} />
             <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </div>
