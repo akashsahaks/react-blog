a6cad39cffe6d48f18151f4de7b2d83e99f63013
akash aks <akashsah.aks@gmail.com>
2023-02-24 12:06:52 +0530

    create page handle controlled input

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label>Blog Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+
+        <label> Blo Author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+        <p> {title} </p>
+        <p> {body} </p>
+        <p> {author} </p>
+      </form>
     </div>
   );
 };
