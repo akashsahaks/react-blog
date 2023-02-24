05b3b63d35298349b22c88eb7ce842db78ce3517
akash aks <akashsah.aks@gmail.com>
2023-02-24 11:00:33 +0530

    setup react routing using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./Home";
+import Create from "./Create";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <div className="App">
         <Navbar />
         <div className="content">
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
         </div>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -6,17 +7,8 @@ const Navbar = () => {
       <nav className="navbar">
         <h1>The React Blog</h1>
         <div className="links">
-          <a href="/">Home</a>
-          <a
-            href="/create"
-            style={{
-              color: "white",
-              backgroundColor: "#f1356d",
-              borderRadius: "8px",
-            }}
-          >
-            New Blog
-          </a>
+          <Link to="/">Home</Link>
+          <Link to="/create">New Blog</Link>
         </div>
       </nav>
     </>
