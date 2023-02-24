f20064f4c306f5640f71e7a72542e8a3020235ab
akash aks <akashsah.aks@gmail.com>
2023-02-24 12:31:19 +0530

    maked post request and added isPending for button

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,23 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("");
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const blog = { title, body, author };
     // console.log(blog);
+
+    setIsPending(true);
+
+    fetch(`http://localhost:8000/blogs`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then((response) => {
+      console.log("new blog added");
+      setIsPending(false);
+    });
   };
 
   return (
@@ -35,10 +47,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
-        <p> {title} </p>
-        <p> {body} </p>
-        <p> {author} </p>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog... </button>}
       </form>
     </div>
   );
