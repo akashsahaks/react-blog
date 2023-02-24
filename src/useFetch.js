f413e7a3e39952613a8231c074650777548dd368
akash aks <akashsah.aks@gmail.com>
2023-02-24 11:16:51 +0530

    setup useEffect cleanup function in useFetch.js file thats

--- src/useFetch.js
+++ src/useFetch.js
@@ -8,13 +8,15 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortController = new AbortController();
+
     //  In real life sometimes fetching data takes time for sometimes, at that time our
     //  loading component show as much time takes server to respond data after responding loading
     //  compoennt gets unload, so here our api does not taking time to load data from server so just to
     //  demonstrate a loading component how its working that's why we use setTimeout for 1 sec delay
 
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortController.signal })
         .then((respose) => {
           // console.log(respose);
           if (!respose.ok) {
@@ -29,11 +31,16 @@ const useFetch = (url) => {
           setError(null);
         })
         .catch((err) => {
-          console.log(err.message);
-          setIsPending(false);
-          setError(err.message);
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            // console.log(err.message);
+            setIsPending(false);
+            setError(err.message);
+          }
         });
     }, 1000);
+    return () => abortController.abort();
   }, [url]);
   return { data, isPending, error };
 };
